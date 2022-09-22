8f3d8692eea11e4e4ed82e8608cd026b37cec65a
Goder-0 <goder0@naver.com>
2022-09-22 14:37:58 +0900

    ApiService

--- src/routes/mainpage.js
+++ src/routes/mainpage.js
@@ -3,6 +3,8 @@ import TimeTable from "../components/TimeTable";
 import Small_info from "../components/Small_info";
 import { UserTableProvider } from "../context/UserTableContext";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { call } from "../service/ApiService";
 
 const Component_position = styled.div `
     display: flex;
@@ -19,16 +21,25 @@ const Left_component = styled.div `
 `;
 
 
+
 function MainPage(){
+    const [list,setList]=useState([]);
+    useEffect(
+        call("/api/mainLecturelist","GET",null).then((response)=>
+        setList(response.data))
+    );
+
 
     return (
         <Component_position>
+            
             <UserTableProvider>
                 <Right_component>
                     <Piechart />
                 </Right_component>
                 <Left_component>
                     <Small_info />
+                    {list.length}
                     <TimeTable width={1000} height={600} />
                 </Left_component>
             </UserTableProvider>
