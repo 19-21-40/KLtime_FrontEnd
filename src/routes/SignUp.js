6e17252c6e8984e01228739eac329765488a09be
jaemin-shin02 <sour_jam0220@naver.com>
2022-09-28 19:33:37 +0900

    commit

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -4,16 +4,16 @@ import UserInfoProvider from "../context/UserInfoContext";
 import { call } from "../service/ApiService";
 
 function SignUp(){
-    const [user,setUser]=useState();
-    useEffect(
-        call("/auth/sign_up/","POST",
-        {
-            name:"이성훈",
-            number:"2019203029",
-            email:"skqnrua5123@naver.com",
-            password:"1234"
-        }).then((response)=>setUser(response))
-        ,[]);
+    // const [user,setUser]=useState();
+    // useEffect(
+    //     call("/auth/sign_up/","POST",
+    //     {
+    //         name:"이성훈",
+    //         number:"2019203029",
+    //         email:"skqnrua5123@naver.com",
+    //         password:"1234"
+    //     }).then((response)=>setUser(response))
+    //     ,[]); //백엔드 코드 아직 연결 X
     return (
         <div>
             <UserInfoProvider>
