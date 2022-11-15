6809fee75adb53ec25407d79025cbbe698059a6b
jaemin-shin02 <sour_jam0220@naver.com>
2022-11-15 22:40:39 +0900

    강의 삭제시 디테일 수정

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -427,7 +427,8 @@ function TimeTable({
     const previewLecture = userTable.searchedLectures.find(lecture => lecture.id == userTable.previewId)
     const selectedLectures = userTable.totalTimeTable.find(timeTable => timeTable.id === userTable.selectedId).lectureList;
     
-    const onClick = (id, color) => {
+    const onClick = (id, color, event) => {
+        event.stopPropagation();
         setClickedLecture({
             ...selectedLectures.find(lecture => lecture.id === id),
             backgroundColor: color,
