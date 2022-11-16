a76a3c842976dfd068fd943ab50874fb0b1c4f53
jaemin-shin02 <sour_jam0220@naver.com>
2022-11-16 15:25:28 +0900

    commit

---
+++ src/components/Edit_info.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState, useCallback } from "react";
+import styled from "styled-components";
+
+
+const Total_Container = styled.div`
+
+`;
+
+const Info_line = styled.div`
+
+`;
+
+const Pw_line = styled.div`
+
+`;
+
+const Department_line = styled.div`
+
+`;
+
+const Edit_Btn = styled.button`
+
+`;
+
+const Confirm_Btn = styled.button`
+
+`;
+
+const Close_Btn = styled.button`
+
+`;
+
+function Edit_info({}){
+
+    return(
+        <Total_Container>
+            
+        </Total_Container>
+    );
+}
+
+export default Edit_info;
