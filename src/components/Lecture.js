610ccfab8e23393e9c1115f84c23804c8f579e47
jaemin-shin02 <sour_jam0220@naver.com>
2022-11-19 17:07:02 +0900

    detail_click

--- src/components/Lecture.js
+++ src/components/Lecture.js
@@ -1,82 +1,82 @@
 //추가
 import { useEffect, useState } from "react";
-import styled ,{css}  from "styled-components";
+import styled, { css } from "styled-components";
 import { useUserTableState } from "../context/UserTableContext";
 
 //styled-components
-const DualMode=styled.div`
+const DualMode = styled.div`
   ${props =>
-    props.isCardMode ?
-    css`
+        props.isCardMode ?
+            css`
         // position: absolute; //리스트에서 카드모드일때는 안되게 해야함
         overflow: hidden;
         cursor: pointer;
         box-shadow: rgb(15 15 15 / 5%) 0px 0px 0px 1px, rgb(15 15 15 / 5%) 0px 2px 4px;
-        width:${props=>props.width};
-        height:${props=>props.height};
+        width:${props => props.width};
+        height:${props => props.height};
 
         align-items:center;
-        top:${props=>props.top};
+        top:${props => props.top};
         left: 0%;
         border-radius: 5px;
         margin-left: 0px;
         color: rgb(255, 255, 255);
-        background-color:${props=>props.backgroundColor};
+        background-color:${props => props.backgroundColor};
         opacity: 1;
         z-index: 0;
-        ${props=>props.isListMode?`position:relative;`:`position:absolute;`}; //추가 
+        ${props => props.isListMode ? `position:relative;` : `position:absolute;`}; //추가 
     `
-    :
-    css`
+            :
+            css`
 
     `
-  }
+    }
 `;
-const ContentBox=styled(DualMode)`
+const ContentBox = styled(DualMode)`
     ${props =>
         props.blockhover ?
 
-        (props.isCardMode ?
-        css`
+            (props.isCardMode ?
+                css`
             height:100%;
             border-radius:5px;
             background-color:${props.backgroundColor};
 
             &:hover{background-color:rgba(55,53,47,0.25);}
         `
-        :
-        css`
-            // backgroundColor:white;
-            ${props=>
-                props.clicked?
-                `background-color:rgba(190, 190, 191, 0.8);`
                 :
-                `background-color:${props.backgroundColor};
+                css`
+            // backgroundColor:white;
+            ${props =>
+                        props.clicked ?
+                            `background-color:rgba(190, 190, 191, 0.8);`
+                            :
+                            `background-color:${props.backgroundColor};
                 &:hover{background-color:#f8f8f8;}`
-            };
+                    };
             // &:hover{background-color:#f8f8f8;}
             // 수정해야함
             &:active{background-color:rgba(190, 190, 191, 0.8);}
         `)
-        :
-        (props.isCardMode ?
-            css`
+            :
+            (props.isCardMode ?
+                css`
                 height:100%;
                 border-radius:5px;
                 background-color:${props.backgroundColor};
     
                 //&:hover{background-color:rgba(55,53,47,0.25);}
             `
-            :
-            css`
+                :
+                css`
                 // backgroundColor:white;
-                ${props=>
-                    props.clicked?
-                    `background-color:rgba(190, 190, 191, 0.8);`
-                    :
-                    `background-color:${props.backgroundColor};
+                ${props =>
+                        props.clicked ?
+                            `background-color:rgba(190, 190, 191, 0.8);`
+                            :
+                            `background-color:${props.backgroundColor};
                     &:hover{background-color:#f8f8f8;}`
-                };
+                    };
                 // &:hover{background-color:#f8f8f8;}
                 // 수정해야함
                 &:active{background-color:rgba(190, 190, 191, 0.8);}
@@ -84,17 +84,17 @@ const ContentBox=styled(DualMode)`
     }
 `;
 
-const Content=styled(ContentBox)`
+const Content = styled(ContentBox)`
     ${props =>
         props.isCardMode ?
-        css`
+            css`
             position:relative; //추가
             overflow: hidden;
             padding: 1px 0 0 3px;
             font-size: 12px;
         `
-        :
-        css`
+            :
+            css`
             /* 추가 */
             border-top: 1px solid rgba(167, 168, 169, 0.8);
             padding: 12px 0 12px 10px;
@@ -114,7 +114,7 @@ const Content=styled(ContentBox)`
         }
     }
 `;
-const LectureDelBtn=styled.button`
+const LectureDelBtn = styled.button`
 
     background-color: transparent;
     border:none;
@@ -136,82 +136,85 @@ function Lecture({
     top,
     backgroundColor = "rgb(255, 187, 59)",
     isCardMode,
-    isListMode=false,//수정
+    isListMode = false,//수정
     onClick,
     onDeleteClick,
     onHovered,
     isClicked
 }) {
-    const [clicked,setClicked]=useState(false);
-    const state=useUserTableState();
+    const [clicked, setClicked] = useState(false);
+    const state = useUserTableState();
 
-    useEffect(()=>{
+    useEffect(() => {
         setClicked(isClicked);
-    },[state.selectedId,isClicked])
+    }, [state.selectedId, isClicked])
 
+    const LectureClick = () => {
+        if(!clicked){
+            onClick();
+            setClicked(true);
+        }
+        if(!isListMode){
+            onClick();
+        }
+    }
 
     return (
         <DualMode isCardMode={isCardMode} width={width} height={height} top={top} backgroundColor={backgroundColor}
             onMouseEnter={onHovered}
             onMouseLeave={onHovered}
-            onClick={
-                clicked?()=>{}:()=>{
-                onClick();
-                setClicked(true);
-                }
-                
-            }
-            >
+            onClick={LectureClick}
+        >
             <ContentBox isCardMode={isCardMode} clicked={clicked} backgroundColor={backgroundColor} blockhover={blockhover} >
                 <Content isCardMode={isCardMode}>
-                    {isListMode?
-                    <>
-                    <div>
-                       <strong>{lecture.lectureName}</strong>
-                       {blockhover && <LectureDelBtn onClick={(event)=>{
-                           onDeleteClick(lecture.id)
-                           event.stopPropagation()
-                           }}>x</LectureDelBtn>}
-                   </div>
-                   <div>
-                       {lecture.professor}
-                   </div>
-                   <div>
-                       {lecture.department}
-                   </div>
-                   <div>
-                       {lecture.lectureTimes.map((lectureTime,index) => (
-                           <span key={index}>
-                               {`${lectureTime.day} ${lectureTime.startTime} ${lectureTime.endTime}`}
-                           </span>
-                       ))}
-                   </div>
-                   <div>
-                       {lecture.level}
-                   </div>
-                   <div>
-                       {lecture.section}
-                   </div>
-                   <div>
-                       {lecture.credit}
-                   </div>
-                   <div style={{ display: isCardMode ? 'none' : 'block'}}>
-                       {lecture.notes}
-                   </div>
-                   </>
-                     :
-                     <>
-                     {blockhover && <LectureDelBtn onClick={(event)=>{
-                            onDeleteClick(lecture.id)
-                            event.stopPropagation()
+                    {isListMode ?
+                        <>
+                            <div>
+                                <strong>{lecture.lectureName}</strong>
+                                {blockhover && <LectureDelBtn onClick={(event) => {
+                                    onDeleteClick(lecture.id)
+                                    event.stopPropagation()
+                                }}>x</LectureDelBtn>}
+                            </div>
+                            <div>
+                                {lecture.professor}
+                            </div>
+                            <div>
+                                {lecture.department}
+                            </div>
+                            <div>
+                                {lecture.lectureTimes.map((lectureTime, index) => (
+                                    <span key={index}>
+                                        {`${lectureTime.day} ${lectureTime.startTime} ${lectureTime.endTime}`}
+                                    </span>
+                                ))}
+                            </div>
+                            <div>
+                                {lecture.level}
+                            </div>
+                            <div>
+                                {lecture.section}
+                            </div>
+                            <div>
+                                {lecture.credit}
+                            </div>
+                            <div style={{ display: isCardMode ? 'none' : 'block' }}>
+                                {lecture.notes}
+                            </div>
+                        </>
+                        :
+                        <>
+                            {blockhover && <LectureDelBtn onClick={(event) => {
+                                onDeleteClick(lecture.id)
+                                event.stopPropagation()
                             }}>x</LectureDelBtn>}
-                    <div style={{marginTop: "6px" ,fontWeight: 900}}>
-                        {lecture.lectureName}
-                    </div>
-                    <div>
-                        {lecture.professor}
-                    </div>
-                    </>
+                            <div style={{ marginTop: "6px", fontWeight: 900 }}>
+                                {lecture.lectureName}
+                            </div>
+                            <div>
+                                {lecture.professor}
+                            </div>
+                        </>
                     }
                 </Content>
             </ContentBox>
